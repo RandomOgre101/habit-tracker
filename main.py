a97282c0944bd0bf4f6b7d5a8bc69688abf7352d
Rishikeshav Ravichandran <94991501+RandomOgre101@users.noreply.github.com>
2023-06-04 17:57:41 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,45 @@
+import requests
+from datetime import datetime
+
+USERNAME = 'daxsteel'
+TOKEN = "hiejdeindhskj"
+
+pixela_endpoint = "https://pixe.la/v1/users"
+user_params = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes"
+}
+
+# r = requests.post(url=pixela_endpoint, json=user_params)
+# print(r.text)
+
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+graph_config = {
+    "id": "rishicode",
+    "name": "Coding Graph",
+    "unit": "hours",
+    "type": "float",
+    "color": "momiji"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN
+}
+
+# r = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(r.text)
+
+pixel_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/rishicode"
+
+today = datetime.now().strftime("%Y%m%d")
+
+pixel_config = {
+    "date": today,
+    "quantity": input("Enter hours: "),
+}
+
+r = requests.post(url=pixel_endpoint, json=pixel_config, headers=headers)
+print(r.text)
